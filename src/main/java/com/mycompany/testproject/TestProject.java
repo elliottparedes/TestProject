8766998328b2bdf000b2f02a24481033ddaf276d
ElliottParedes <ElliottParedes@yahoo.com>
2022-10-19 14:45:44 -0500

    initial

---
+++ src/main/java/com/mycompany/testproject/TestProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.testproject;
+
+/**
+ *
+ * @author Ellio
+ */
+public class TestProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
